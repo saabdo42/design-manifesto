4dad47967bf3a274fe8872b9172f59926d7b523d
saabdo42 <saabdo42@tntech.edu>
2021-12-01 17:37:19 -0600

    update
    added content and variables

--- script.js
+++ script.js
@@ -1,87 +1,94 @@
 var canv = document.getElementById("canv");
 var ctx = canv.getContext("2d");
 
-var box0 = document.getElementById("zero");
-var box1 = document.getElementById("A1");
-var box2 = document.getElementById("A2");
-var box3 = document.getElementById("A3");
-var box4 = document.getElementById("A4");
-var box5 = document.getElementById("A5");
-var box6 = document.getElementById("A6");
-var box7 = document.getElementById("A7");
-var box8 = document.getElementById("A8");
-var box9 = document.getElementById("A9");
-var box10 = document.getElementById("A10");
-var box11 = document.getElementById("A11");
-var box12 = document.getElementById("A12");
-var box13 = document.getElementById("A13");
+var zero = document.getElementById("zero"); //ALL 20 STEPS OF PATH A NOT INCLUDING ART
+var A1 = document.getElementById("A1");
+var A1_1 = document.getElementById("A1.1");
+var A2 = document.getElementById("A2");
+var A2_1 = document.getElementById("A2.1");
+var A3 = document.getElementById("A3");
+var A4 = document.getElementById("A4");
+var A4_1 = document.getElementById("A4.1");
+var A4_2 = document.getElementById("A4.2");
+var A4_3 = document.getElementById("A4.3");
+var A5 = document.getElementById("A5");
+var A6 = document.getElementById("A6");
+var A7 = document.getElementById("A7");
+var A8 = document.getElementById("A8");
+var A9 = document.getElementById("A9");
+var A10 = document.getElementById("A10");
+var A10_1 = document.getElementById("A10.1");
+var A11 = document.getElementById("A11");
+var A12 = document.getElementById("A12");
+var A13 = document.getElementById("A13");
 
-var art1
-var art2
-var art3
-var art4 = document.getElementById("Z4");
+var B1 = document.getElementById("B1"); //ALL 5 STEPS OF PATH B NOT INCLUDING ART
+var B2 = document.getElementById("B2");
+var B3 = document.getElementById("B3");
+var B4 = document.getElementById("B4");
+var B5 = document.getElementById("B5");
+
+var C1 = document.getElementById("C1"); //PATH C LOL
+
+var M1 = document.getElementById("M1"); //ALL MANIFESTO STAEMENTS
+var M2 = document.getElementById("M2");
+var M3 = document.getElementById("M3");
+var M4 = document.getElementById("M4");
+
+var Z1 = document.getElementById("Z1"); //ALL ART RESULTS
+var Z2 = document.getElementById("Z2");
+var Z3 = document.getElementById("Z3");
+var Z4 = document.getElementById("Z4");
 
 var thisbox
+var prevbox
 
 function canvsize(){    //RESIZES CANVAS TO WINDOW
-  ctx.canvas.width  = window.innerWidth;
+  ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight; 
 }
 
-function checkbox(thing){      //CHECKS IF INPUTTED THING IS INVISIBLE
+function isinvis(thing){    //CHECKS IF INPUTTED THING IS INVISIBLE
   return thing.classList.contains("invis")
 }
 
-function reveal(thing){        //REVEALS INPUTTED THING
+function reveal(thing){   //REVEALS INPUTTED THING
   thing.classList.remove("invis");
   thing.classList.add("vis");
+  prevbox = thisbox;
   thisbox = thing;
 }
 
 function show(){ //SHOWS ALL BOXES IN ORDER
 
-  if(checkbox(box1) == true){
-    reveal(box1);
-
-  }else if (checkbox(box2) == true){
-    reveal(box2);
-
-  }else if (checkbox(box3) == true){
-    reveal(box3);
-
-  }else if (checkbox(box4) == true){
-    reveal(box4);
-  
-  }else if (checkbox(box5) == true){
-    reveal(box5);
-
-  }else if (checkbox(box6) == true){
-    reveal(box6);
-  
-  }else if (checkbox(box7) == true){
-    reveal(box7);
-  
-  }else if (checkbox(box8) == true){
-    reveal(box8);
-  
-  }else if (checkbox(box9) == true){
-    reveal(box9);
-  
-  }else if (checkbox(box10) == true){
-    reveal(box10);
-  
-  }else if (checkbox(box11) == true){
-    reveal(box11);
-  
-  }else if (checkbox(box12) == true){
-    reveal(box12);
-  
-  }else if (checkbox(box13) == true){
-    reveal(box13);
-  
-  }else if (checkbox(art4) == true){
-    reveal(art4);
-
+  if(isinvis(A1) == true){
+    reveal(A1);
+  }else if (isinvis(A2) == true){
+    reveal(A2);
+  }else if (isinvis(A3) == true){
+    reveal(A3);
+  }else if (isinvis(A4) == true){
+    reveal(A4);
+  }else if (isinvis(A5) == true){
+    reveal(A5);
+  }else if (isinvis(A6) == true){
+    reveal(A6);
+  }else if (isinvis(A7) == true){
+    reveal(A7);
+  }else if (isinvis(A8) == true){
+    reveal(A8);
+  }else if (isinvis(A9) == true){
+    reveal(A9);
+  }else if (isinvis(A10) == true){
+    reveal(A10);
+  }else if (isinvis(A11) == true){
+    reveal(A11);
+  }else if (isinvis(A12) == true){
+    reveal(A12);
+  }else if (isinvis(A13) == true){
+    reveal(A13);
+  }else if (isinvis(Z4) == true){
+    reveal(Z4);
   }
 }
 
@@ -89,9 +96,9 @@ function place(event) {
   var x = event.clientX;
   var y = event.clientY;
   
-  if ( checkbox(box2) == true ){  //ON FIRST CLICK
-    var startx = box0.style.top + 50; //START POINT IS BOX ZERO
-    var starty = box0.style.left + 50;
+  if ( isinvis(A2) == true ){  //ON FIRST CLICK
+    var startx = zero.style.top + 50; //START POINT IS BOX ZERO
+    var starty = zero.style.left + 50;
 
     var bruhx = x; //NEXT START POINT WILL BE BOX 1 POSITION
     var bruhy = y;
