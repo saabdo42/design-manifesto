f3cfce6b603d87d777caa385c1f2a3318092387f
saabdo42 <saabdo42@tntech.edu>
2021-12-02 14:17:50 -0600

    update but broken
    lines are not working gonna try to fix it but saving this version

--- script.js
+++ script.js
@@ -41,7 +41,8 @@ var Z3 = document.getElementById("Z3");
 var Z4 = document.getElementById("Z4");
 
 var thisbox
-var prevbox
+var lastbox
+var lastlastbox
 
 function canvsize(){    //RESIZES CANVAS TO WINDOW
   ctx.canvas.width = window.innerWidth;
@@ -55,7 +56,6 @@ function isinvis(thing){    //CHECKS IF INPUTTED THING IS INVISIBLE
 function reveal(thing){   //REVEALS INPUTTED THING
   thing.classList.remove("invis");
   thing.classList.add("vis");
-  prevbox = thisbox;
   thisbox = thing;
 }
 
@@ -63,57 +63,123 @@ function show(){ //SHOWS ALL BOXES IN ORDER
 
   if(isinvis(A1) == true){
     reveal(A1);
+    lastbox = zero;
   }else if (isinvis(A2) == true){
     reveal(A2);
+    lastbox = A1;
+    lastlastbox = zero;
   }else if (isinvis(A3) == true){
     reveal(A3);
+    lastbox = A2;
+    lastlastbox = A1;
   }else if (isinvis(A4) == true){
     reveal(A4);
+    lastbox = A3;
+    lastlastbox = A2;
   }else if (isinvis(A5) == true){
     reveal(A5);
+    lastbox = A4;
+    lastlastbox = A3;
   }else if (isinvis(A6) == true){
     reveal(A6);
+    lastbox = A5;
+    lastlastbox = A4;
   }else if (isinvis(A7) == true){
     reveal(A7);
+    lastbox = A6;
+    lastlastbox = A5;
   }else if (isinvis(A8) == true){
     reveal(A8);
+    lastbox = A7;
+    lastlastbox = A6;
   }else if (isinvis(A9) == true){
     reveal(A9);
+    lastbox = A8;
+    lastlastbox = A7;
   }else if (isinvis(A10) == true){
     reveal(A10);
+    lastbox = A9;
+    lastlastbox = A8;
   }else if (isinvis(A11) == true){
     reveal(A11);
+    lastbox = A10;
+    lastlastbox = A9;
   }else if (isinvis(A12) == true){
     reveal(A12);
+    lastbox = A11;
+    lastlastbox = A10;
   }else if (isinvis(A13) == true){
     reveal(A13);
+    lastbox = A12;
+    lastlastbox = A11;
   }else if (isinvis(Z4) == true){
     reveal(Z4);
+    lastbox = A13;
+    lastlastbox = A12;
   }
 }
 
-function place(event) {
-  var x = event.clientX;
+function place(event){
+  var x = event.clientX; //POSITION OF THE MOUSECLICK
   var y = event.clientY;
   
-  if ( isinvis(A2) == true ){  //ON FIRST CLICK
-    var startx = zero.style.top + 50; //START POINT IS BOX ZERO
-    var starty = zero.style.left + 50;
-
-    var bruhx = x; //NEXT START POINT WILL BE BOX 1 POSITION
-    var bruhy = y;
-  }else{    //ON ALL OTHER CLICKS
-    startx = bruhx; //START POINT IS BRUH X
-    starty = bruhy;
-  }
-
-  thisbox.style.top = y - 50;
+  thisbox.style.top = y - 50; //PLACING THISBOX AT THE MOUSECLICK
   thisbox.style.left = x - 50;
 
-  ctx.moveTo(startx, starty);
-  ctx.strokeStyle = "#97B2F0";
-  ctx.lineWidth = 4;
-  ctx.lineTo(x, y);
-  ctx.stroke();
+  var lastboxx = lastbox.style.left; //DEFINING LAST BOX POSITION AND BOX BEFORE THAT
+  var lastboxy = lastbox.style.top;
+  var lastlastboxx = lastlastbox.style.left;
+  var lastlastboxy = lastlastbox.style.top;
+
+//  zero.innerHTML = "this is stupid";
+
+  if (thisbox.classList.contains("halfstep") == true){ //IF THISBOX IS A BRANCH/ HALFSTEP
+    ctx.moveTo(lastlastboxx, lastlastboxy); //DRAW THE LINE FROM THE LAST LAST BOX TO CURRENT MOUSECLICK 
+    ctx.strokeStyle = "#97B2F0";
+    ctx.lineWidth = 4;
+    ctx.lineTo(x, y);
+    ctx.stroke();
+
+  }else if(thisbox.classList.contains("step") == true){ //OTHERWISE IF THIS IS A FULL STEP
+    ctx.moveTo(lastboxx, lastboxy); //DRAW THE LINE FROM THE LAST BOX TO CURRENT MOUSECLICK 
+    ctx.strokeStyle = "#97B2F0";
+    ctx.lineWidth = 4;
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+  
 }
 
+/*
+function place(event){
+  var x = event.clientX; //POSITION OF THE MOUSECLICK
+  var y = event.clientY;
+  
+  thisbox.style.top = y - 50; //PLACING THISBOX AT THE MOUSECLICK
+  thisbox.style.left = x - 50;
+
+  var lastboxx = thisbox.previousElementSibling.style.left; //DEFINING LAST BOX AND BOX BEFORE THAT
+  var lastboxy = thisbox.previousElementSibling.style.top;
+
+  var lastlastboxx = lastboxx.previousElementSibling.style.left;
+  var lastlastboxy = lastboxy.previousElementSibling.style.top;
+
+  zero.innerHTML = "this is stupid";
+
+  if (thisbox.classList.contains("halfstep") == true){ //IF THISBOX IS A BRANCH/ HALFSTEP
+    ctx.moveTo(lastlastboxx, lastlastboxy); //DRAW THE LINE FROM THE LAST LAST BOX TO CURRENT MOUSECLICK 
+    ctx.strokeStyle = "#97B2F0";
+    ctx.lineWidth = 4;
+    ctx.lineTo(x, y);
+    ctx.stroke();
+
+  }else if(thisbox.classList.contains("step") == true){ //OTHERWISE IF THIS IS A FULL STEP
+    ctx.moveTo(lastboxx, lastboxy); //DRAW THE LINE FROM THE LAST BOX TO CURRENT MOUSECLICK 
+    ctx.strokeStyle = "#97B2F0";
+    ctx.lineWidth = 4;
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+
+}
+*/
