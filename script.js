e3499fa57b02a352c702916f49b344cd3be60c7a
saabdo42 <saabdo42@tntech.edu>
2021-12-02 18:02:12 -0600

    final broken version
    canvas is not working so i'll just disconnect the branches completely

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var canv = document.getElementById("canv");
+var canv2 = document.getElementById("canv2");
 var ctx = canv.getContext("2d");
+var ctx2 = canv.getContext("2d");
 
 var zero = document.getElementById("zero"); //ALL 20 STEPS OF PATH A NOT INCLUDING ART
 var A1 = document.getElementById("A1");
@@ -40,13 +42,16 @@ var Z2 = document.getElementById("Z2");
 var Z3 = document.getElementById("Z3");
 var Z4 = document.getElementById("Z4");
 
-var thisbox
-var lastbox
-var lastlastbox
+var thisbox =zero;
+var lastbox = zero;
+var lastlastbox =zero;
 
 function canvsize(){    //RESIZES CANVAS TO WINDOW
   ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight; 
+
+  ctx2.canvas.width = window.innerWidth;
+  ctx2.canvas.height = window.innerHeight; 
 }
 
 function isinvis(thing){    //CHECKS IF INPUTTED THING IS INVISIBLE
@@ -60,6 +65,8 @@ function reveal(thing){   //REVEALS INPUTTED THING
 }
 
 function show(){ //SHOWS ALL BOXES IN ORDER
+
+  /*
 
   if(isinvis(A1) == true){
     reveal(A1);
@@ -116,10 +123,83 @@ function show(){ //SHOWS ALL BOXES IN ORDER
     reveal(Z4);
     lastbox = A13;
     lastlastbox = A12;
-  }
+  } 
+  
+  */
 }
 
 function place(event){
+
+  //REVEALS EACH BOX IN ORDER 
+  if(isinvis(A1) == true){
+    reveal(A1);
+    lastbox = zero;
+    lastlastbox = zero;
+  }else if (isinvis(A1_1) == true){
+    reveal(A1_1)
+    lastbox = A1;
+    lastlastbox = zero;
+  }else if (isinvis(A2) == true){
+    reveal(A2);
+    lastbox = A1_1;
+    lastlastbox = A1;
+  }else if (isinvis(A2_1) == true){
+    reveal(A2_1);
+    lastbox = A2;
+    lastlastbox = A1_1
+  }else if (isinvis(A3) == true){
+    reveal(A3);
+    lastbox = A2_1;
+    lastlastbox = A2;
+
+
+  }else if (isinvis(A4) == true){
+    reveal(A4);
+    lastbox = A3;
+    lastlastbox = A2;
+  }else if (isinvis(A5) == true){
+    reveal(A5);
+    lastbox = A4;
+    lastlastbox = A3;
+  }else if (isinvis(A6) == true){
+    reveal(A6);
+    lastbox = A5;
+    lastlastbox = A4;
+  }else if (isinvis(A7) == true){
+    reveal(A7);
+    lastbox = A6;
+    lastlastbox = A5;
+  }else if (isinvis(A8) == true){
+    reveal(A8);
+    lastbox = A7;
+    lastlastbox = A6;
+  }else if (isinvis(A9) == true){
+    reveal(A9);
+    lastbox = A8;
+    lastlastbox = A7;
+  }else if (isinvis(A10) == true){
+    reveal(A10);
+    lastbox = A9;
+    lastlastbox = A8;
+  }else if (isinvis(A11) == true){
+    reveal(A11);
+    lastbox = A10;
+    lastlastbox = A9;
+  }else if (isinvis(A12) == true){
+    reveal(A12);
+    lastbox = A11;
+    lastlastbox = A10;
+  }else if (isinvis(A13) == true){
+    reveal(A13);
+    lastbox = A12;
+    lastlastbox = A11;
+  }else if (isinvis(Z4) == true){
+    reveal(Z4);
+    lastbox = A13;
+    lastlastbox = A12;
+  }
+
+  // DRAWS LINES AND DETERMINES POSITION OF BOX
   var x = event.clientX; //POSITION OF THE MOUSECLICK
   var y = event.clientY;
   
@@ -131,55 +211,52 @@ function place(event){
   var lastlastboxx = lastlastbox.style.left;
   var lastlastboxy = lastlastbox.style.top;
 
-//  zero.innerHTML = "this is stupid";
+  ctx.strokeStyle = "#97B2F0";
+  ctx.lineWidth = 4;
 
-  if (thisbox.classList.contains("halfstep") == true){ //IF THISBOX IS A BRANCH/ HALFSTEP
-    ctx.moveTo(lastlastboxx, lastlastboxy); //DRAW THE LINE FROM THE LAST LAST BOX TO CURRENT MOUSECLICK 
-    ctx.strokeStyle = "#97B2F0";
-    ctx.lineWidth = 4;
-    ctx.lineTo(x, y);
-    ctx.stroke();
-
-  }else if(thisbox.classList.contains("step") == true){ //OTHERWISE IF THIS IS A FULL STEP
-    ctx.moveTo(lastboxx, lastboxy); //DRAW THE LINE FROM THE LAST BOX TO CURRENT MOUSECLICK 
-    ctx.strokeStyle = "#97B2F0";
-    ctx.lineWidth = 4;
+  if (thisbox.classList.contains("halfstep") == true){
+    ctx2.beginPath();
+    ctx2.moveTo(lastboxx, lastboxy);
+    ctx2.lineTo(x, y);
+    ctx2.stroke();
+  
+  }else if (thisbox.classList.contains("step") == true){
+    ctx.beginPath
+    ctx.moveTo(lastboxx, lastboxy);
     ctx.lineTo(x, y);
     ctx.stroke();
   }
   
-}
 
-/*
-function place(event){
-  var x = event.clientX; //POSITION OF THE MOUSECLICK
-  var y = event.clientY;
-  
-  thisbox.style.top = y - 50; //PLACING THISBOX AT THE MOUSECLICK
-  thisbox.style.left = x - 50;
+  /*
+  if (lastbox.classList.contains("halfstep") == true){ //IF LAST BOX WAS A BRANCH/ HALFSTEP
+    console.log("-------");
+    console.log("thisbox:" + thisbox.id);
+    console.log("lastbox:" + lastbox.id);
+    console.log(lastboxx + "," + lastboxy);
+    console.log("lastlastbox:" +lastlastbox.id);
+    console.log(lastlastboxx + "," + lastlastboxy);
 
-  var lastboxx = thisbox.previousElementSibling.style.left; //DEFINING LAST BOX AND BOX BEFORE THAT
-  var lastboxy = thisbox.previousElementSibling.style.top;
-
-  var lastlastboxx = lastboxx.previousElementSibling.style.left;
-  var lastlastboxy = lastboxy.previousElementSibling.style.top;
-
-  zero.innerHTML = "this is stupid";
-
-  if (thisbox.classList.contains("halfstep") == true){ //IF THISBOX IS A BRANCH/ HALFSTEP
+    ctx.beginPath();
     ctx.moveTo(lastlastboxx, lastlastboxy); //DRAW THE LINE FROM THE LAST LAST BOX TO CURRENT MOUSECLICK 
-    ctx.strokeStyle = "#97B2F0";
-    ctx.lineWidth = 4;
     ctx.lineTo(x, y);
+    console.log("Drawing from " + lastlastboxx + "," + lastlastboxy + " to " + x + "," + y);
     ctx.stroke();
 
-  }else if(thisbox.classList.contains("step") == true){ //OTHERWISE IF THIS IS A FULL STEP
+  }else if(lastbox.classList.contains("step") == true){ //OTHERWISE IF IT WAS A FULL STEP
+    console.log("-------");
+    console.log("thisbox:" + thisbox.id);
+    console.log("lastbox:" + lastbox.id);
+    console.log(lastboxx + "," + lastboxy);
+    console.log("lastlastbox:" +lastlastbox.id);
+    console.log(lastlastboxx + "," + lastlastboxy);
+
+    ctx.beginPath();
     ctx.moveTo(lastboxx, lastboxy); //DRAW THE LINE FROM THE LAST BOX TO CURRENT MOUSECLICK 
-    ctx.strokeStyle = "#97B2F0";
-    ctx.lineWidth = 4;
     ctx.lineTo(x, y);
+    console.log("Drawing from " + lastboxx + "," + lastboxy + " to " + x + "," + y);
     ctx.stroke();
   }
-
+  */
+  
 }
-*/
